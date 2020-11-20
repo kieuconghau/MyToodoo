e1b5e3a9768484bdf430fccb6770ee935dd76ca5
18127259 <46527188+kieuconghau@users.noreply.github.com>
2020-11-21 06:46:28 +0700

    Add files via upload
    Features:
    - Add a task.
    - Edit a task.
    - Remove a task.
    - Local storage.
    - Responsive design.

---
+++ script.js
@@ -0,0 +1,97 @@
+// Global variables declaration
+var boxAddTask = document.getElementById('box-add-task');
+var btnAddTask = document.getElementById('btn-add-task');
+var taskList = document.getElementById('task-list');
+var mode = "add";   // "add", "edit"
+var editIDNum;
+
+// Invoke functions
+loadTasksFromLocalStorage();
+boxAddTask.focus();
+btnAddTask.setAttribute('onclick', 'addBtnEvent(boxAddTask.value)');
+
+
+// Functions
+function loadTasksFromLocalStorage() {
+    for (let i = 0; i < window.localStorage.length; ++i) {
+        addTaskItemNode(window.localStorage.getItem(i.toString()), i);
+    }
+}
+
+function addBtnEvent(oldContent) {
+    if (mode == "add") {
+        let content = boxAddTask.value;
+        let idNum = window.localStorage.length;
+        boxAddTask.value = '';
+
+        if (content != '') {
+            addTaskItemNode(content, idNum);
+            window.localStorage.setItem(idNum, content);
+        }
+    }
+    else if (mode == "edit") {        
+        let content = boxAddTask.value;
+        let idNum = editIDNum;
+        boxAddTask.value = '';
+
+        if (content == '') {
+            content = oldContent;
+        }
+
+        window.localStorage.setItem(idNum, content);
+        mode = "add";
+    }
+}
+
+function addTaskItemNode(content, idNum) {
+    if (content != '') {
+        var taskItem = document.createElement('div');
+        taskItem.className = 'task-item';
+        taskItem.id = idNum.toString();
+        
+        var taskContent = document.createElement('p');
+        taskContent.className = 'task-content';
+        taskContent.setAttribute('onclick', 'editTask(' + taskItem.id + ')')
+        taskContent.appendChild(document.createTextNode(content));
+
+        var btnRemoveTask = document.createElement('button');
+        btnRemoveTask.className = 'btn-remove-task';
+        btnRemoveTask.setAttribute('onclick', 'removeTask(' + taskItem.id + ')');
+        btnRemoveTask.appendChild(document.createTextNode('X'));
+
+        taskItem.appendChild(taskContent);
+        taskItem.appendChild(btnRemoveTask);
+
+        taskList.appendChild(taskItem);
+    }
+}
+
+function removeTask(idNum) {
+    document.getElementById(idNum.toString()).remove();
+    window.localStorage.setItem(idNum, "");
+    if (mode == 'edit' && idNum == editIDNum) {
+        mode = 'add';
+        location.reload();
+    }
+}
+
+function editTask(idNum) {
+    if (editIDNum != null && idNum != editIDNum) {
+        document.getElementById(editIDNum.toString()).firstElementChild.style.backgroundColor = "#FFF8E8";
+    }
+
+    mode = "edit";
+    editIDNum = idNum;
+    var taskContent = document.getElementById(idNum.toString()).firstElementChild;
+    boxAddTask.value = taskContent.innerHTML;
+    boxAddTask.focus();
+    btnAddTask.innerHTML = 'Edit';
+    btnAddTask.style.backgroundColor = "#FFC75F";
+    btnAddTask.onmouseover = () => {
+        btnAddTask.style.backgroundColor = "#FF9671";
+    }
+    btnAddTask.onmouseout = () => {
+        btnAddTask.style.backgroundColor = "#FFC75F";
+    }
+    taskContent.style.backgroundColor = "#FFEECA";
+}
